94dd99fcdce207dd9483e3a7751a18f15bd43a95
Gaurav Patil <89126032+SirusnakJR1872000@users.noreply.github.com>
2024-07-25 22:34:57 -0500

    code for array.py

--- code for array.py
+++ code for array.py
@@ -1,107 +1,132 @@
-# code for array 
-def insert(arr, n, length, capacity):
-    # for inserting we need to make sure that we have space in our array
-    # and to ensure that there is space in the array the length of the array should be less than the capacity of array
+# code for static array
+# we will performing some basic function like inserting and deleting an element at the end of the array and also from the middle of the array
+
+# the variables required would be:
+# arr: array
+# i: position
+# n: element to be inserted 
+# length: number of elements
+# capacity: size of the array
+
+# lets start with the first function
+
+# here we will insert the element at the end of the array
+
+def insertEnd(arr, n, length, capacity):
+    # now we need to check if there is some space in the array
+    # for that the length of the array should be less than that of the capacity
     if length < capacity:
         arr[length] = n
-        # inserting the value at that particular length
-        # after inserting we have to increase the value of length since we have added an element to array
+        # here we have inserted the value at the end of the array
+        # after this we need to increment the length since we just inserted an element
         return length + 1
-    # if there is no space we will normally return the length
     return length
 
-# if we want to delete the element from the array
-def delete(arr, length):
-    # we need to check if the array is empty or not 
-    # that means the length should be greater than 0
-    if length > 0:
-        arr[length - 1] = 0
-        # here we set a default value to 0 for the last element because we don't really delete the element we just keep it ready for overwriting
-        return length - 1
-    return length
+# now lets perfrom insertion in the middle of the array
 
-# if we want to insert element in the middle to the array
-def insertmiddle(arr, n, length, capacity, i):
-    # again for insertion we need to make sure that the array has space in it
+def insertMiddle(arr, n, i, length, capacity):
+    # now lets check if there is space in the array
     if length < capacity:
-        for index in range(length - 1, i -1, -1):
+        # we need to shift the elements to the right to make room for the new element in the middle of the array
+        for index in range(length - 1, i - 1, -1):
+            # we are moving from back hence we are decrementing our counter
             arr[index + 1] = arr[index]
-            #here each element is moved to the right to make space for the element to be inserted
+            # here we have shfted the element to the right by incrementing the pointer and now we will insert the element at the desired place
         arr[i] = n
-        #here we inserted the elemnent at the specified position
-        # so now we have to increase the length by 1 and return the value
-        return length + 1
+        # now we will increment the length since we inserted a new element
+        return length + 1 
     return length
 
-# suppose we want to delete a value from the middle of the array
-def removemiddle(arr, i, length):
-    # to remove an element we need to make sure that the index is within the bounds of array
-    if i < length:
-        # we need to shift the elements to the left to overwrite the position of the deleted element
-        for index in range(i + 1, length):
-            # we will go to the element specified to be removed and we will shift it to the left
-            # so here we assign the index of the previous element to the next element 
-            arr[index - 1] = arr[index]
+def deleteEnd(arr, length):
+    # for deleting an element we need to make sure that the array is not empty
+    # we can do that by checking if length of the array is greater than 0
+    if length > 0:
+        # now we have to assign value 0 which is a default value to the end of the array so that we can overwrite it later
+        arr[length - 1] = 0
+        # now we just have to decrement the value of length by 1 since we deleted an element 
         return length - 1
     return length
 
-def printarray(arr, capacity):
+def deleteMiddle(arr, i, length):
+    # we need to check if the specified position is within the range of the array
+    if i < length:
+        # if this condition is satisfies then we can shift the elements towards the left
+        for index in range(i + 1, length):
+            arr[index - 1] = arr[index]
+            # so we just assigned a previous position to the next element of the specified position
+        return length - 1 
+    return length
+
+# now lets print the array
+def printArray(arr, capacity):
     for i in range(capacity):
-        print(arr[i], end='')
+        print(arr[i], end= '')
     print()
 
-# main function to get values from a user
+# now its time for main function so that we can allow user to insert their specific values
 
 def main():
-    #get the capacity of array
-    capacity = int(input("Enter the capacity of array : "))
-    # now we need to create a array with specified capacity and we will assign 'capacity' no. of zeroes indicating fixed nature of arrays and 0 is a default value
+
+    # lets intake the capacity of the array
+
+    capacity = int(input("Enter the capacity of the array :"))
+    
+    # now after taking the capacity we can assign the 'capacity' number of zeroes and as we know zero is a default value and can be overwritten
+
     arr = [0] * capacity
-    # now set the initial length to 0 indicating that the array is empty for now
+
+    # lets assign length to be zero as well
+
     length = 0
+
+    # writing a menu for user to choose the desired operation
 
     while True:
         print("\n Menu ")
-        print("1. Insert element at the end of array ")
-        print("2. Insert element at the middle of array ")
-        print("3. Remove element from the end of the array ")
-        print("4. Remove element from the middle of array ")
-        print("5. Print the array ")
-        choice = int(input("Enter your choice ; "))
+        print(" Insert element at the end of the array")
+        print(" Insert element in the middle of the array")
+        print(" Delete element at the end of the array")
+        print(" Delete element in the middle of the array")
+        print(" Print the Array ")
+        choice = int(input("Enter your choice: "))
 
         if choice == 1:
-            n = int(input("Enter the value to be inserted : "))
-            length = insert(arr, n, length, capacity)
+            n = int(input("Enter the element to be inserted at the end of the array: "))
+            length = insertEnd(arr, n, length, capacity) # call the function
 
         elif choice == 2:
-            i = int(input("Enter the position at which the element is to be inserted: "))
             n = int(input("Enter the element to be inserted: "))
-            # now we have to check whether the entered index is a valid index or not
-            # for that index to be valid it should lie between 0 and the length of array
+            i = int(input("Enter the position at which the elemwnt is to be inserted: "))
+            # now we have to check if the entered value is in the range of the array
+            # we can do that checking if it lies between 0 and length and the actual array
             if 0 <= i <= length:
-                length = insertmiddle(arr, n, length, capacity, i)
-            else: 
-                print("Invalid index. Please try again")
+                length = insertMiddle(arr, n, i, length, capacity) # call the function
+            else:
+                print("Enter a valid choice.")
 
         elif choice == 3:
-            length = delete(arr, length)
+            length = deleteEnd(arr, length) # call the function
 
         elif choice == 4:
-            i = int(input("Enter the index from which the element is to be removed: "))
-            # now we need to check if it is a valid index
-            # for the index to be valid it should lie between 0 and the length of array
+            i = int(input("Enter the position at which the value is supposed to be deleted"))
+            # now we have to check if it is within the bound sof the array
             if 0 <= i <= length:
-                length = removemiddle(arr, i, length)
+                length = deleteMiddle(arr, i, length) # call the function
             else:
-                print("Invalid index. Please try again")
-            
+                print("Enter a valid choice")
+        
         elif choice == 5:
-            print("Array : ", end = '')
-            printarray(arr, capacity)
-
+            print("Array :", end = '')
+            printArray(arr, capacity) # call the function
+        
         else:
-            print("Invalid Input. Please try again.")
+            print("Please enter a valid choice.")
 
 if __name__ == "__main__":
-    main()
+        main()
+    
+            
+
+
+
 
