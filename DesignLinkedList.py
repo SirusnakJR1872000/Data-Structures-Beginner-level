6119f72887f99e0466f1194f2cdafc0bcf063532
Gaurav Patil <89126032+SirusnakJR1872000@users.noreply.github.com>
2024-07-29 16:11:58 -0500

    DesignLinkedList.py

--- DesignLinkedList.py
+++ DesignLinkedList.py
@@ -20,32 +20,64 @@
 # Output
 # [null, null, null, null, 2, null, 3]
 
+# Design Linked List
+
+# Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+# A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+# If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+# Implement the MyLinkedList class:
+
+# MyLinkedList() Initializes the MyLinkedList object.
+# int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+# void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+# void addAtTail(int val) Append a node of value val as the last element of the linked list.
+# void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
+# void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
+ 
+# Example 1:
+# Input
+# ["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+# [[], [1], [3], [1, 2], [1], [1], [1]]
+# Output
+# [null, null, null, null, 2, null, 3]
+
 class MyLinkedList:
 
     def __init__(self):
-        self.ls = []
+        # lets initialize a list
+        self.list = []
+        
 
     def get(self, index: int) -> int:
-        return self.ls[index] if index < len(self.ls) else -1
-
+        # now we will check if the index is within the bounds of the list otherwise we return -1
+        return self.list[index] if index < len(self.list) else -1
+        
     def addAtHead(self, val: int) -> None:
-        self.ls.insert(0, val)
-
+        # we will use insert() and the head will always be at 0 so we insert at 0
+        self.list.insert(0,val)
+       
     def addAtTail(self, val: int) -> None:
-        self.ls.append(val)
+        # we can use the append() to add at the end of the list
+        self.list.append(val)
+        
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index <= len(self.ls):
-            self.ls.insert(index, val)
-
+        # we need t0 check if the index is within the bounds of the list
+        # if yes then we will insert using insert() at that index 
+        if index <= len(self.list):
+            self.list.insert(index,val)
+        
     def deleteAtIndex(self, index: int) -> None:
-        if index in range(len(self.ls)):
-            self.ls.pop(index)
-
+        # we will see if the index is in range
+        if index <= len(self.list):
+            self.list.pop(index) 
         
 
 
-# Your MyLinkedList object will be instantiated and called as such:
+
+
+        # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
 # param_1 = obj.get(index)
 # obj.addAtHead(val)
