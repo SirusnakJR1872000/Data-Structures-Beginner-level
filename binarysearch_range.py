33afb2e7a5a276f9585326bb427228f9b7f499e5
Gaurav Patil <89126032+SirusnakJR1872000@users.noreply.github.com>
2024-08-09 15:53:38 -0500

    Add files via upload

---
+++ binarysearch_range.py
@@ -0,0 +1,32 @@
+# lets write a code for binary search for a range of values
+# here we will specify a condition to check if the value is within the range or not
+
+def binarySearch(low, high):
+
+    while low <= high:
+
+        mid = (low + high) // 2
+
+        if (isCorrect(mid) > 0):
+            high = mid - 1
+        
+        elif (isCorrect(mid) < 0):
+            low = mid + 1
+
+        else:
+            return mid
+        
+    return -1
+
+
+# helper fucntion to mention the condition
+def isCorrect(n):
+
+    if n > 10:
+        return 1
+    
+    elif n < 10:
+        return -1
+    
+    else:
+        return 0
